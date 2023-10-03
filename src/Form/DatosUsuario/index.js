60247b69618a8fd6bd88f76f307dee5c26093a9d
Gonza <gonzavillalba123@gmail.com>
2023-10-03 11:47:46 -0300

    Realizando las validaciones de el Formulario de Datos Usuario

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -1,41 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./validaciones";
 
-class DatosUsuario extends React.Component {
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    );
-  }
+const DatosUsuario = () => {
+
+  const [email, setEmail] = useState({
+    value: "",
+    valid: null
+  })
+
+  const [password, setPassword] = useState({
+    value: "",
+    valid: null
+  })
+
+  const [errorEmail, setErrorEmail] = useState(false)
+  const [errorPassword, setErrorPassword] = useState(false)
+
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={ (e) => {
+          e.preventDefault();
+          if(email.valid && password.valid){
+            console.log("siguiente formulario")
+            console.log(email, password)
+          }else{
+            console.log("no hacer nada")
+          }
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={errorEmail}
+        helperText={errorEmail && "El correo electronico debe tener entre 8 y 50 caracteres, ademas de contener '@'"}
+        value = { email.value }
+        onChange={ (e) => {
+          setEmail({value: e.target.value, valid: true})
+          setErrorPassword(false)
+        } }
+        onBlur={
+          (e) => {
+            setEmail({value: e.target.value, valid: validarEmail(e.target.value)})
+            setErrorEmail(!validarEmail(e.target.value))
+          }
+        }
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        error={errorPassword}
+        helperText={errorPassword && "La contraseña debe tener entre 8 y 20 caracteres"}
+        value = { password.value }
+        onChange={ (e) => {
+          setPassword({value: e.target.value, valid: true})
+          setErrorPassword(false)
+        } }
+        onBlur={
+          (e) => {
+            setPassword({value: e.target.value, valid: validarPassword(e.target.value)})
+            setErrorPassword(!validarPassword(e.target.value))
+          }
+        }
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
 }
 
 export default DatosUsuario;
--- src/Form/styles.js
+++ src/Form/styles.js
@@ -13,6 +13,12 @@ export const LogoSpace = styled.div`
   align-items: center;
   justify-content: center;
   padding-bottom: 5rem;
+  @media (max-width: 768px) {
+    padding: 2rem 0 2rem 0;
+    margin-bottom: 2rem;
+    background-color: #e61919;
+    border-radius: 5px;
+  }
 `;
 
 export const Img = styled.img`
